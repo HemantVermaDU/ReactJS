0a8e8b25315923863651cdef614f3feb74181b8d
Hemant Verma <41631241+HemantVermaDU@users.noreply.github.com>
2021-07-14 22:02:03 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Apples=()=> {
+    return (<div className="Apples">
+    <h1 style={{textAlign : "center",color:"pink",fontSize:"100px"}}>Hello World !!</h1> 
+    <h2 style={{color:"white"}}>Hello EveryOne</h2> 
+    <button className="button">Button 1</button>
+    <button className="button">Button 2</button>
+    <button className="button">Button 3</button>
+    </div>);
+}
+export default Apples;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import Apples from "./App.js"
+import "./style.css"
+
+ReactDOM.render( <Apples/>, document.getElementById("root"))
